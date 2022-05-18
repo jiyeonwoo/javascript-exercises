5e441604ce4b618c1d950110d03856c9d6452d38
Ji Yeon Woo <jiyeonwoo@berkeley.edu>
2022-05-17 20:36:14 -0700

    finish 8

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    let output = 0;
+    if (start < 0 || end < 0 || typeof(start) != 'number' || typeof(end) != 'number') {
+        return 'ERROR';
+    }
+    if (start < end) {
+        for (let i = start; i <= end; i++) {
+            output+=i;
+        }
+    } else {
+        for (let i = end; i <= start; i++) {
+            output+=i;
+        }
+    }
+    return output;
 };
 
 // Do not edit below this line
--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(input) {
+  return input.reduce((a, b) => add(a, b), 0);
 };
 
-const multiply = function() {
-
+const multiply = function(input) {
+  return input.reduce((a, b) => a*b, 1);
 };
 
-const power = function() {
-	
+const power = function(a, power) {
+  let out = 1;
+  for (let i = 0; i < power; i++) {
+    out *= a;
+  }
+  return out;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	let out = 1;
+  for (let i = 1; i < a+1; i++) {
+    out *= i;
+  }
+  return out;
 };
 
 // Do not edit below this line
